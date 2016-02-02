09c48e00938aa964041ecd0a4037bcc4047b69ba
Ray Alez <raymestalez@gmail.com>
2016-02-01 23:59:52 -0500

    Update.

--- webcomics/posts/views.py
+++ webcomics/posts/views.py
@@ -384,7 +384,7 @@ class UserFeed(Feed):
     
 
     def item_description(self, item):
-        return "http://webcomics.io/media/"+str(item.image)
+        return "<img src='http://webcomics.io/media/'"+str(item.image)+"/>"
 
 
 
@@ -414,7 +414,7 @@ class SeriesFeed(Feed):
     
 
     def item_description(self, item):
-        return "http://webcomics.io/media/"+str(item.image)
+        return "<img src='http://webcomics.io/media/'"+str(item.image)+"/>"
 
 
 
