a3f2a6559d3403588142167e22278e3e0c78a8de
Ray Alez <raymestalez@gmail.com>
2016-02-06 11:46:50 -0500

    Update.

--- webcomics/posts/forms.py
+++ webcomics/posts/forms.py
@@ -7,7 +7,7 @@ from series.models import Series
 from multiupload.fields import MultiFileField
 
 class PostForm(ModelForm):
-    images = MultiFileField(min_num=0, max_num=10, max_file_size=1024*1024*5, required=False)
+    images = MultiFileField(min_num=0, max_num=30, max_file_size=1024*1024*5, required=False)
     
 
     class Meta:
