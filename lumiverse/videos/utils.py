a8e068b70c2ac7e45b857d799a4e6a5935a057d3
Ray Alez <raymestalez@gmail.com>
2016-03-14 11:54:29 -0400

    Change gravity, video_id in utils.

--- lumiverse/videos/utils.py
+++ lumiverse/videos/utils.py
@@ -4,9 +4,33 @@ from django.utils.timezone import utc
 from functools import reduce
 from django.db import models
 
+# for youtube id
+from urllib.parse import urlparse, parse_qs
+
+def video_id(value):
+    """
+    Examples:
+    - http://youtu.be/SA2iWivDJiE
+    - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    - http://www.youtube.com/embed/SA2iWivDJiE
+    - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    """
+    query = urlparse(value)
+    if query.hostname == 'youtu.be':
+        return query.path[1:]
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = parse_qs(query.query)
+            return p['v'][0]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+    # fail?
+    return None
 
 
-def hot_score(post, gravity=1.1, timebase=120): # gravity=1.8
+def hot_score(post, gravity=1.8, timebase=120): # gravity=1.8
     rating = (post.score + 1)**0.8
     now = datetime.datetime.utcnow().replace(tzinfo=utc)
     age = int((now - post.pub_date).total_seconds())/60
--- lumiverse/videos/views.py
+++ lumiverse/videos/views.py
@@ -29,32 +29,8 @@ from hubs.models import Hub
 from .forms import VideoForm
 from .models import Video
 
-from .utils import rank_hot
+from .utils import rank_hot,video_id
 
-# for youtube id
-from urllib.parse import urlparse, parse_qs
-
-def video_id(value):
-    """
-    Examples:
-    - http://youtu.be/SA2iWivDJiE
-    - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
-    - http://www.youtube.com/embed/SA2iWivDJiE
-    - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
-    """
-    query = urlparse(value)
-    if query.hostname == 'youtu.be':
-        return query.path[1:]
-    if query.hostname in ('www.youtube.com', 'youtube.com'):
-        if query.path == '/watch':
-            p = parse_qs(query.query)
-            return p['v'][0]
-        if query.path[:7] == '/embed/':
-            return query.path.split('/')[2]
-        if query.path[:3] == '/v/':
-            return query.path.split('/')[2]
-    # fail?
-    return None
 
 class BrowseMixin(object):
     paginate_by = 15
