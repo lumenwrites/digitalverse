d731c4ff06471a1ba3926a65532ebe5ee48b2a11
Ray Alez <raymestalez@gmail.com>
2016-09-09 22:16:36 -0400

    lumen rig.

--- lumiverse/lumiverse/urls.py
+++ lumiverse/lumiverse/urls.py
@@ -15,7 +15,8 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^about/', views.about),    
+    url(r'^about/', views.about),
+    url(r'^lumen/', views.lumen),        
 
     url(r'', include(profiles_urls)),        
     # url(r'', include(posts_urls)),
--- lumiverse/lumiverse/views.py
+++ lumiverse/lumiverse/views.py
@@ -9,3 +9,7 @@ def home(request):
 def about(request):
     return render(request, 'about.html', {
     })
+
+def lumen(request):
+    return render(request, 'lumen.html', {
+    })
